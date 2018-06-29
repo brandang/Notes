1254f12adf8b9131b88bc60e86700892ac3385f4
Brandon <ilikevid3ogames@gmail.com>
2018-06-28 21:40:43 -0400

    Created initial files

---
+++ src/Constants.java
@@ -0,0 +1,8 @@
+/**
+ * A class that stores constants for the program.
+ */
+public class Constants {
+
+    // The name of the Window.
+    final public static String WINDOW_TITLE = "Notes";
+} 
---
+++ src/Launcher.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Launcher class that starts the program.
+ */
+public class Launcher extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle(Constants.WINDOW_TITLE);
+    }
+}
